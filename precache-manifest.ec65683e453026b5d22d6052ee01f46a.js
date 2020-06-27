4b0366797c69b9ecd9b10f2938cf97c4c3bc6b4e
Ugni Blanc <ajo2185@gmail.com>
2020-06-27 12:34:37 -0700

    Updates

---
+++ precache-manifest.ec65683e453026b5d22d6052ee01f46a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a478c31c5dc03344e855ec949c54583",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "18b646df211102cd0960",
+    "url": "/clickygame/static/css/main.4157eff9.chunk.css"
+  },
+  {
+    "revision": "374cd20b749c6d2b3585",
+    "url": "/clickygame/static/js/2.5d2e3be2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.5d2e3be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b646df211102cd0960",
+    "url": "/clickygame/static/js/main.fb4f2ecb.chunk.js"
+  },
+  {
+    "revision": "9b713f51294d0326cdcc",
+    "url": "/clickygame/static/js/runtime-main.854f56c0.js"
+  }
+]);
